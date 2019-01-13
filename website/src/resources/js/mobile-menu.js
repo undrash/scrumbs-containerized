809f8ec938afb9f3e9f22ce4038678acec5b7983
Andrei Gaspar <andrei@planeeet.com>
2019-01-13 17:59:52 +0200

    - Testing IOS response on full screen change

--- website/src/resources/js/mobile-menu.js
+++ website/src/resources/js/mobile-menu.js
@@ -11,17 +11,16 @@ if ( document.getElementById( "navbar-menu-btn" ) ) {
 
     hamburgerMenu.addEventListener( "click", () => {
         mobileMenu.style.display = "block";
-        document.documentElement.requestFullscreen();
         document.documentElement.classList.add( "hide-page-overflow" );
         document.body.classList.add( "hide-page-overflow" );
+        document.documentElement.requestFullscreen();
     });
 
 
     closeMobileMenu.addEventListener( "click", () => {
         mobileMenu.style.display = "none";
-        document.exitFullscreen();
         document.documentElement.classList.remove( "hide-page-overflow" );
         document.body.classList.remove( "hide-page-overflow" );
-
+        document.exitFullscreen();
     });
 }
